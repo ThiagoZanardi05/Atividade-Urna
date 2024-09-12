6d9e9f7c0b9276bdff2afa170cd7b484f69fceac
ThiagoZanardi05 <thiagozanardi05@gmail.com>
2024-09-11 21:07:26 -0300

    Criação entidade Eleitor

--- urna/src/main/java/com/urna/urna/Entity/Eleitor.java
+++ urna/src/main/java/com/urna/urna/Entity/Eleitor.java
@@ -1,4 +1,39 @@
 package com.urna.urna.Entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "eleitores")
 public class Eleitor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    private String nome;
+
+    @CPF
+    private String cpf;
+
+    @NotBlank
+    private String profissao;
+
+    @NotBlank
+    private String telefoneCelular;
+
+    @NotBlank
+    private String telefoneFixo;
+
+    @Email
+    private String email;
 }
