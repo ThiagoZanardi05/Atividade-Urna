f829155852251368523690e325da5772132852d6
ThiagoZanardi05 <thiagozanardi05@gmail.com>
2024-09-16 20:32:36 -0300

    Arrumei o save de eleitor

--- urna/src/main/java/com/urna/urna/Service/EleitorService.java
+++ urna/src/main/java/com/urna/urna/Service/EleitorService.java
@@ -16,14 +16,10 @@
         private EleitorRepository eleitorRepository;
 
         public Eleitor saveEleitor(Eleitor eleitor) {
-            if (eleitor.getCpf() == null || eleitor.getCpf().isBlank()) {
+            if (eleitor.getEmail() == null || eleitor.getEmail().isEmpty() ||
+                    eleitor.getCpf() == null || eleitor.getCpf().isBlank()) {
                 eleitor.setStatus(Status.PENDENTE);
-            }
-
-            if (eleitor.getEmail() == null || eleitor.getEmail().isEmpty()) {
-                eleitor.setStatus(Status.PENDENTE);
-            }
-            else{
+            } else {
                 eleitor.setStatus(Status.APTO);
             }
             return eleitorRepository.save(eleitor);
@@ -48,34 +44,7 @@
             return eleitorRepository.findByStatus(Status.APTO);
         }
 
-        public Eleitor updateEleitor(Long id, Eleitor novosDadosEleitor) {
-            Optional<Eleitor> eleitorExistenteOp = eleitorRepository.findById(id);
 
-            if (eleitorExistenteOp.isPresent()) {
-                Eleitor eleitorExistente = eleitorExistenteOp.get();
-
-                if (eleitorExistente.getStatus() == Status.INATIVO) {
-                    novosDadosEleitor.setStatus(Status.INATIVO);
-                } else {
-                    if (novosDadosEleitor.getCpf() == null || novosDadosEleitor.getCpf().isBlank() ||
-                            novosDadosEleitor.getEmail() == null || novosDadosEleitor.getEmail().isEmpty()) {
-                        novosDadosEleitor.setStatus(Status.PENDENTE);
-                    } else {
-                        novosDadosEleitor.setStatus(Status.APTO);
-                    }
-                }
-
-                eleitorExistente.setProfissao(novosDadosEleitor.getProfissao());
-                eleitorExistente.setTelefoneCelular(novosDadosEleitor.getTelefoneCelular());
-                eleitorExistente.setTelefoneFixo(novosDadosEleitor.getTelefoneFixo());
-                eleitorExistente.setEmail(novosDadosEleitor.getEmail());
-                eleitorExistente.setCpf(novosDadosEleitor.getCpf());
-
-                return eleitorRepository.save(eleitorExistente);
-            } else {
-                throw new RuntimeException("Eleitor não encontrado");
-            }
-        }
     }
 
 
